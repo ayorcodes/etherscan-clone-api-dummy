48ab9cac116715e8739fa50d1fba2ddbf1d16f42
Ayomide <adeayo35@gmail.com>
2021-12-03 07:27:37 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import blocksRouter from './modules/blocks';
 import transactionsRouter from './modules/transactions';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use('/blocks', blocksRouter);
 app.use('/transactions', transactionsRouter);
